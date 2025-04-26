a51bd6c84fc6a28387765f965f04bb5251478fe4
touhid-070 <touhidansari187@gmail.com>
2025-04-26 10:56:08 +0530

    set and dictionary

---
+++ main/sets.py
@@ -0,0 +1,14 @@
+s={3,33,23,55,22}
+
+print(s,type(s))
+print(s.add(355))
+print(s)
+
+# Dictionary
+
+student={"name":"John","age":20,"marks":90,"address":"New York"}
+print(student.values(),type(student))
+print(student.keys())
+print(student.items())
+print(student.get("name"))
+
