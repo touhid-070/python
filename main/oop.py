b254378938b8d81781dfb8b5896c4c14b6bd23b1
touhid-070 <touhidansari187@gmail.com>
2025-04-29 11:31:01 +0530

    Refactor Employee class to include constructor and additional methods for salary and info retrieval

--- main/oop.py
+++ main/oop.py
@@ -1,12 +1,33 @@
+# class Employee:
+#     company="Google"
+
+#     def get_salary(self):
+#         return 1000
+    
+# e = Employee()
+# print(e.get_salary()) 
+
+# e2=Employee()
+# print(e2.get_salary())
+# print(e2.company) 
+
+
+# learning constructors
 class Employee:
-    company="Google"
+    def __init__(self,salaey,name,bond):
+        self.salary=salaey
+        self.name=name
+        self.bond=bond
 
     def get_salary(self):
-        return 1000
+        return self.salary
     
-e = Employee()
-print(e.get_salary()) 
+    def get_info(self):
+        print(f"Name: {self.name}, Salary: {self.salary}, Bond: {self.bond}")
+    
+e1 = Employee(34000,"John",2)
+print(e1.get_salary())
+e1.get_info()
 
-e2=Employee()
-print(e2.get_salary())
-print(e2.company) 
+e2=Employee(50000,"Doe",3)
+e2.get_info()
