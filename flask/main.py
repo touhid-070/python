c6446c343a35db88d9b54713c3844fe8bf4c1eda
touhid-070 <touhidansari187@gmail.com>
2025-05-10 12:29:37 +0530

    Add service, about, and contact pages with navigation to Flask application

--- flask/main.py
+++ flask/main.py
@@ -2,7 +2,19 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 app.run(debug=True)
